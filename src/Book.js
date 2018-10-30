80e5e7633483fbec8afd2fdc622c3e3c769c2dd8
katharineabbott <katharinemabbott@gmail.com>
2018-10-30 14:16:15 -0700

    committing to switch branch

--- src/Book.js
+++ src/Book.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react'
 
 class Book extends Component {
-    state = {
-        bookCategory: "none"
-    }
-
     render() {
         return (
             <li>
@@ -12,7 +8,7 @@ class Book extends Component {
                     <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.url + ')' }}></div>
                     <div className="book-shelf-changer">
-                        <select onChange={(event) => {this.props.handleShelfChange(event)}}>
+                        <select onChange={(event) => {this.props.handleShelfChange(event, this.props.id, this.props.shelf)}}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
--- src/BookCase.js
+++ src/BookCase.js
@@ -7,7 +7,8 @@ class BookCase extends Component {
         noShelf: [],
         currentlyReadingShelf: [],
         wantToReadShelf: [],
-        readShelf: []
+        readShelf: [],
+        bookCategory: ""
     }
 
     populateShelves = () => {
@@ -42,14 +43,13 @@ class BookCase extends Component {
     })
     }
     
-    handleShelfChange = (event) => {
+    handleShelfChange = (event, id, shelf) => {
     let newValue = event.nativeEvent.target.value
-    let oldValue = this.state.bookCategory
-    console.log(oldValue)
     console.log(newValue)
-    if (newValue !== oldValue) {
-        this.setState({bookCategory: newValue})
-    }
+    console.log(id)
+    console.log(shelf)
+    // BooksAPI.update()
+
     
     // this.populateShelves()
     //state needs to apply only to single book
