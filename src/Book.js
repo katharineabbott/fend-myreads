4f719e0a3ec2f674ee1bfe3873eabdb7a59e1247
katharineabbott <katharinemabbott@gmail.com>
2018-10-30 17:32:28 -0700

    drop down list now reflects which shelf book is currently on

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,7 @@ class Book extends Component {
                     <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.url + ')' }}></div>
                     <div className="book-shelf-changer">
-                        <select defaultValue="move" onChange={(event) => {this.shelfChange(this.props.book, event.nativeEvent.target.value)}}>
+                        <select defaultValue={this.props.shelf} onChange={(event) => {this.shelfChange(this.props.book, event.nativeEvent.target.value)}}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
