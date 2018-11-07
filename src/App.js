8fb2c0fcbf7f293a4b603e7ddfe9b5bee5ec46e8
katharineabbott <katharinemabbott@gmail.com>
2018-11-06 17:17:17 -0800

    refactored to prep for search function and created temporary switch to search page in state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,90 @@
 import React from 'react'
 import BookCase from './BookCase'
 import Search from './Search'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    noShelf: [],
+    currentlyReadingShelf: [],
+    wantToReadShelf: [],
+    readShelf: [],
+    screen: "search",
+    query: "",
+    matchingBooks: []
+  }
+
+  populateShelves = () => {
+  let noShelf = this.state.noShelf
+  let currentlyReadingShelf = this.state.currentlyReadingShelf
+  let wantToReadShelf = this.state.wantToReadShelf
+  let readShelf = this.state.readShelf
+  BooksAPI.getAll().then(result => {
+      result.forEach(book => {
+        this.setState({bookCategory: book.shelf})
+        if (book.shelf === 'currentlyReading') {
+            // this.setState({bookCategory: book.shelf})
+            currentlyReadingShelf.push(book)
+        } else if (book.shelf === 'wantToRead') {
+            // this.setState({bookCategory: book.shelf})
+            wantToReadShelf.push(book)
+        } else if (book.shelf === 'read') {
+            // this.setState({bookCategory: book.shelf})
+            readShelf.push(book)
+        } else {
+            // this.setState({bookCategory: book.shelf})
+            noShelf.push(book)
+        }
+      })
+      this.setState({noShelf: noShelf});
+      this.setState({currentlyReadingShelf: currentlyReadingShelf});
+      this.setState({wantToReadShelf: wantToReadShelf});
+      this.setState({readShelf: readShelf});
+    })
+  }
+
+  handleShelfChange = (shelf, book) => {
+      if (book.shelf !== shelf) {
+          book.shelf = shelf
+          this.setState({noShelf: []})
+          this.setState({currentlyReadingShelf: []})
+          this.setState({wantToReadShelf: []})
+          this.setState({readShelf: []})
+          this.populateShelves()
+      } else {
+          alert("This book is already on that shelf!")
+      }
+  }
+
+  updateQuery = (query) => {
+    let matchingBooks = this.state.matchingBooks
+    this.setState({query: query.trim()})
+    BooksAPI.search(query).then((response) => {
+        if (this.state.query.length >= 1) {
+            response.forEach(book => {
+                matchingBooks.push(book)
+            })
+        } else {
+            console.log("No search term")
+        }
+    })
+    this.setState({matchingBooks: matchingBooks})
+    console.log(matchingBooks)
+}
+
+  componentDidMount(){
+    this.populateShelves()
+  }
   render() {
     return (
       <div className="app">
-        <Search/>
-        <BookCase />
+        {this.state.screen === "search" && (
+            <Search updateQuery={this.updateQuery} query={this.state.query} matchingBooks={this.state.matchingBooks} currentlyReading={this.state.currentlyReadingShelf} wantToRead={this.state.wantToReadShelf} read={this.state.readShelf} handleShelfChange={this.handleShelfChange}/>
+         )}
+        {this.state.screen === "bookshelf" && (
+          <BookCase currentlyReading={this.state.currentlyReadingShelf} wantToRead={this.state.wantToReadShelf} read={this.state.readShelf} handleShelfChange={this.handleShelfChange}/>
+        )}
       </div>
     )
   }
--- src/BookCase.js
+++ src/BookCase.js
@@ -1,61 +1,7 @@
 import React, {Component} from 'react'
 import Shelf from './Shelf'
-import * as BooksAPI from './BooksAPI'
 
 class BookCase extends Component {
-    state = {
-        noShelf: [],
-        currentlyReadingShelf: [],
-        wantToReadShelf: [],
-        readShelf: []
-    }
-
-    populateShelves = () => {
-    let noShelf = this.state.noShelf
-    let currentlyReadingShelf = this.state.currentlyReadingShelf
-    let wantToReadShelf = this.state.wantToReadShelf
-    let readShelf = this.state.readShelf
-    BooksAPI.getAll().then(result => {
-        result.forEach(book => {
-        this.setState({bookCategory: book.shelf})
-        if (book.shelf === 'currentlyReading') {
-            // this.setState({bookCategory: book.shelf})
-            currentlyReadingShelf.push(book)
-        } else if (book.shelf === 'wantToRead') {
-            // this.setState({bookCategory: book.shelf})
-            wantToReadShelf.push(book)
-        } else if (book.shelf === 'read') {
-            // this.setState({bookCategory: book.shelf})
-            readShelf.push(book)
-        } else {
-            // this.setState({bookCategory: book.shelf})
-            noShelf.push(book)
-        }
-        })
-        this.setState({noShelf: noShelf});
-        this.setState({currentlyReadingShelf: currentlyReadingShelf});
-        this.setState({wantToReadShelf: wantToReadShelf});
-        this.setState({readShelf: readShelf});
-    })
-    }
-    
-    handleShelfChange = (shelf, book) => {
-        if (book.shelf !== shelf) {
-            book.shelf = shelf
-            this.setState({noShelf: []})
-            this.setState({currentlyReadingShelf: []})
-            this.setState({wantToReadShelf: []})
-            this.setState({readShelf: []})
-            this.populateShelves()
-        } else {
-            alert("This book is already on that shelf!")
-        }
-    }
-
-    componentDidMount(){
-    this.populateShelves()
-    }
-
     render() {
         return (
             <div className="list-books">
@@ -63,9 +9,9 @@ class BookCase extends Component {
                     <h1>MyReads</h1>
                 </div>
                 <div className="list-books-content">
-                    <Shelf title='Currently Reading' bookArray={this.state.currentlyReadingShelf} handleShelfChange={this.handleShelfChange}/>
-                    <Shelf title='Want to Read' bookArray={this.state.wantToReadShelf}  handleShelfChange={this.handleShelfChange}/>
-                    <Shelf title='Read' bookArray={this.state.readShelf} handleShelfChange={this.handleShelfChange}/>
+                    <Shelf title='Currently Reading' bookArray={this.props.currentlyReading} handleShelfChange={this.props.handleShelfChange}/>
+                    <Shelf title='Want to Read' bookArray={this.props.wantToRead}  handleShelfChange={this.props.handleShelfChange}/>
+                    <Shelf title='Read' bookArray={this.props.read} handleShelfChange={this.props.handleShelfChange}/>
                 </div>
                 <div className="open-search">
                     <a>Add a book</a>
